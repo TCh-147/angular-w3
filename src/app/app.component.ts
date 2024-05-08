d310456bb36c178a22f412c59969b7638d845b64
Tedy <tedich-03@abv.bg>
2024-05-08 19:19:52 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,101 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Notes } from '../notes/note.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'angular-w3';
+  public index = 0;
+  public modelTitle = "";
+  public modelNote = "";
+  public notesList: Notes[] = [];
+  public titlError = false;
+  public noteError = false;
+  public isEdited = false;
+
+  public saveNote(){
+   
+    if(this.modelTitle.length < 5)
+      this.titlError = true;
+    else this.titlError = false;
+
+    if(this.modelNote.length < 7)
+      this.noteError = true;
+    else this.noteError = false;
+      
+    if(!this.noteError && !this.titlError) {
+
+      if(this.isEdited){
+        this.notesList[this.index].title = this.modelTitle;
+        this.notesList[this.index].note = this.modelNote;
+        this.resetTempData();
+        this.isEdited = false;
+      }
+      else{
+
+        this.notesList.push({
+          title: this.modelTitle,
+          note: this.modelNote
+        })
+    
+        if(this.index >= 0){
+          this.index++;
+        }
+    
+        this.resetTempData();
+
+      }
+
+    }
+
+  }
+
+  public selectedNote;
+
+  public editNote(id){
+
+    if(this.selectedNote === id){
+      this.selectedNote = -1;
+    }
+    else{
+      this.selectedNote = id;
+    }
+
+  }
+
+  public showData(id){
+
+    this.modelTitle = this.notesList[id].title;
+    this.modelNote = this.notesList[id].note;
+    this.isEdited = true;
+    this.index = id;
+
+  }
+
+  public deleteNote(id){
+
+    this.notesList.splice(id, 1);
+    this.resetTempData();
+    this.isEdited = false;
+    
+  }
+
+  private resetTempData(){
+
+    this.modelTitle = '';
+    this.modelNote = '';
+
+  }
+
+  
 }
+
+
---
+++ src/notes/note.component.ts
@@ -0,0 +1,11 @@
+
+export class Notes{
+    title: string;
+    note: string;
+  
+    constructor(title, note){
+      this.title = title;
+      this.note = note;
+    }
+}
+  
